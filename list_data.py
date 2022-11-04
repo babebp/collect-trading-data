ecd58c3a66b9f498e6a3774433ba4d8bf46f0de7
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-11-05 02:52:18 +0700

    add eth to collect data

--- list_data.py
+++ list_data.py
@@ -1,11 +1,13 @@
 filenames_list = [
         'bnb.csv',
         'bitcoin.csv',
+        'eth.csv',
         'CHZ.csv',
     ]
 
 links_list = [
         'https://coinmarketcap.com/th/currencies/bnb/',
         'https://coinmarketcap.com/th/currencies/bitcoin/',
+        'https://coinmarketcap.com/th/currencies/ethereum/',
         'https://coinmarketcap.com/th/currencies/chiliz/',
     ]
